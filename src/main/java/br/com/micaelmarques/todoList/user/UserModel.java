01063aaea29ece984ed5148f69e520688a127b3b
micaelmarques <micaelf.yo18@gmail.com>
2025-03-26 16:26:28 -0300

    primeiro teste

---
+++ src/main/java/br/com/micaelmarques/todoList/user/UserModel.java
@@ -0,0 +1,13 @@
+package br.com.micaelmarques.todoList.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    
+    public String username;
+    public String name;
+    public String password;
+
+   
+}
