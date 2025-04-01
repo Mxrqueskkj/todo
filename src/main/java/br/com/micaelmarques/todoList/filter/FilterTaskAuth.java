7e5256b6e6a1969b1782898dfda605d2d57760d9
micaelmarques <micaelf.yo18@gmail.com>
2025-04-01 17:09:22 -0300

    erro corrigido, agora implementando mais funçoes no fktro e nas tasks

--- src/main/java/br/com/micaelmarques/todoList/filter/FilterTaskAuth.java
+++ src/main/java/br/com/micaelmarques/todoList/filter/FilterTaskAuth.java
@@ -1,26 +1,27 @@
 package br.com.micaelmarques.todoList.filter;
 
 import java.io.IOException;
-import java.nio.file.DirectoryStream.Filter;
-import java.rmi.ServerException;
-
+import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import org.springframework.stereotype.Component;
 
-public class FilterTasksAuth implements Filter{
-    
+@Component
+public class FilterTaskAuth implements Filter {
+
     @Override
-    public boolean accept(Object entry) throws IOException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'accept'");
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
+        System.out.println("Chegou no filtro");
+        chain.doFilter(request, response);
     }
-    
-    /*
+    /*   
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
         throws IOException, SecurityException{
             System.out.println("Chegou no filtro");
             chain.doFilter(request, response);
-        }*/
+        } */
 }
--- src/main/java/br/com/micaelmarques/todoList/task/ITaskRepository.java
+++ src/main/java/br/com/micaelmarques/todoList/task/ITaskRepository.java
@@ -3,7 +3,10 @@ package br.com.micaelmarques.todoList.task;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID>{
-    
+
+    List<TaskModel> findByIdUser(UUID idUser);
 }
